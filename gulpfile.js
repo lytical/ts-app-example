c0190efa4b1f3cddb75f8604ba8b7ae8daa5791d
barry hayles <barry.hayles@lytical.com>
2026-01-29 17:07:11 -0600

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,4 @@ const _gulp = require('gulp');
 const _pump = require('pump');
 
 exports.post_build = (done) =>
-  _pump(
-    _gulp.src(['.env', '.env.*', 'package.json', 'README.md']),
-    _gulp.dest('./dist'),
-    done,
-  );
+  _pump(_gulp.src(['.env', '.env.*', 'README.md']), _gulp.dest('./dist'), done);
